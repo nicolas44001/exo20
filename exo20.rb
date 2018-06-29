1504f0eb2fcc1075aded6b566161831d79d00f60
nicolas44001 <40698059+nicolas44001@users.noreply.github.com>
2018-06-29 19:01:37 +0200

    Update exo20.rb

--- exo20.rb
+++ exo20.rb
@@ -1,10 +1,10 @@
 puts "Salut, bievenue dans ma super pyramide ! Combien d'étages veux-tu ? (choisir un nombre entre 1 et 25)"
 print ">"
 nombre = gets.chomp.to_i
-if nombre>25
+while nombre>25 || nombre<1 do 
   puts "error : choisir un nombre entre 1 et 25"
 end
-if nombre<25
+if nombre<25 && nombre>1
 puts "voici la pyramide :"
 end
 x=1
